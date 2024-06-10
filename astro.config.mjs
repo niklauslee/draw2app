cf1d33787c19d80bc2e1cd7f9052ca021514c8c7
Minkyu Lee <niklaus.lee@gmail.com>
2024-06-10 12:44:24 +0900

    setup tailwind react shadcn-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "server",
+  adapter: cloudflare(),
+});
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="bg-green-100">
+      <Button>button</Button>
+    </div>
+  );
+}
